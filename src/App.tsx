e8b0900eb5ac6a5725db34524257cc7144fb3069
Szymon17 <jablonski.szymon21@gmail.com>
2023-04-21 13:26:45 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,16 @@ import ProgressBar from "./components/Progress-bar/Progress-bar.component";
 function App() {
   const { setQuestions, questions } = useContext(QuestionsContext);
   const [question, setQuestion] = useState<string | null>(null);
-  const [lastQuestion, setLastQuestion] = useState<string | null>(null);
   const [countingState, setCountingState] = useState(false);
+  const [questionInCounting, setQuestionInCounting] = useState("");
 
   useEffect(() => {
     if (QUESTIONS_DATA) setQuestions(QUESTIONS_DATA.questions);
   }, []);
+
+  useEffect(() => {
+    if (question === questionInCounting) setQuestion(null);
+  }, [questionInCounting]);
 
   const getRandomQuestion = () => {
     const saveGuard = 100;
@@ -37,13 +41,13 @@ function App() {
   };
 
   const startCounting = () => {
-    if (question !== null && question !== lastQuestion) {
-      setLastQuestion(question);
+    if (question !== null && !countingState) {
       setCountingState(false);
 
       setTimeout(() => setCountingState(true), 10);
+
       setTimeout(() => {
-        if (countingState) setQuestion(null);
+        setQuestionInCounting(question);
       }, 5000);
     }
   };
@@ -52,13 +56,13 @@ function App() {
     <div className="App">
       <div className="app-container">
         <QuestionCard question={question} />
+        <ProgressBar isOn={countingState} />
         <div className="button_container">
           <Button buttonType={BUTTON_CLASSES.blue} onClick={rollQuestion}>
             Losuj pytanie
           </Button>
           <Button onClick={startCounting}>Start</Button>
         </div>
-        <ProgressBar isOn={countingState} />
       </div>
     </div>
   );
